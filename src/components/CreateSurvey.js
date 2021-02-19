4fce29e82435305bde65092876c5e1ee0870dfaa
heatblst <ashishkumar07865apk@gmail.com>
2021-02-19 14:22:08 +0530

    commit on master

--- src/components/CreateSurvey.js
+++ src/components/CreateSurvey.js
@@ -68,8 +68,6 @@ function CreateSurvey(props) {
             onChange={(event) => {
               setslct(true);
               setval(event.target.value);
-              if (event.target.value === "single-select")
-                setarray([{ text: "Yes" }, { text: "No" }]);
             }}
           >
             <option value="do-select">Select Question Type</option>
